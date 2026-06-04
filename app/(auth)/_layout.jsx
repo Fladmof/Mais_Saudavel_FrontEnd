62db01a3d82c829385516073a758098ccc48dc71
unknown <misaelcagiba812@gmail.com>
2026-06-05 00:39:39 +0100

    lts

--- app/(auth)/_layout.jsx
+++ app/(auth)/_layout.jsx
@@ -1,17 +1,14 @@
+/*
+import {View, Text} from 'react-native';
+import React from 'react';
 
-import { Stack } from 'expo-router';
-
-export default function AuthLayout() { 
+const AuthLayout = () => {
     return (
-        <Stack>
-            <Stack.Screen 
-                name="sign-in" 
-                options={{ headerShown: false }}
-            />
-            <Stack.Screen 
-                name="sign-up" 
-                options={{ headerShown: false }}
-            />
-        </Stack>
-    );
+        <View>
+            <Text>authlayout</Text>
+        </View>
+    )
 }
+
+export default AuthLayout;
+*/
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,7 @@
-// app/_layout.tsx
-import { Stack } from 'expo-router';
+/*
+import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return (
-    <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-    </Stack>
-  );
+  return <Stack screenOptions={{headerShown: false}}/>;
 }
+*/
